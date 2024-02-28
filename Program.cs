eb259c88bb9cf51da90f536bb23776dde9e735c2
FT9R <ft9r@mail.ru>
2024-02-28 09:04:56 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿var streamReader = StreamReader.Null;
+var samplesPath = Path.Combine(Environment.CurrentDirectory, "samples.csv");
+var consoleKey = ConsoleKey.None;
+
+/* App description */
+Console.WriteLine("This is a console application designed to find the best-fit linear equation 'yi = axi + b'\n" +
+    $"for a samples dataset(xi,yi) provided by the user in: \n\"{samplesPath}\" file.");
+Console.WriteLine("Where 'xi' and 'yi' are independent and dependent values from the dataset, respectively.\n" +
+    "'a' is the slope of the line and 'b' is the y-intercept.");
+Console.WriteLine("The sample file should be located in the same directory as this application and contain \n" +
+    "data separated by a dot between the integer and fractional parts and a comma between \nthe values as follows:");
+Console.WriteLine("- - -\nx1,y1\nx2,y2\n. . .\nxn,yn\n- - -\n");
+
+while (true)
+{
+    List<double> independentValues = [];
+    List<double> dependentValues = [];
+
+    try
+    {
+        streamReader = new StreamReader(samplesPath);
+    }
+    catch (Exception e)
+    {
+        ExceptionHandler(e);
+
+        continue;
+    }
+    GetValuesFromSamplesSet(independentValues, dependentValues);
+    streamReader.Close();
+
+    try
+    {
+        var (slope, intercept) = Statistics.LeastSquares.GetSlopeAndIntercept([.. independentValues], [.. dependentValues]);
+        Console.WriteLine("Samples processed:\t{0}", independentValues.Count);
+        Console.WriteLine("Line slope:\t\t{0:N6}", slope);
+        Console.WriteLine("y-intercept:\t\t{0:N6}", intercept);
+    }
+    catch (Exception e)
+    {
+        ExceptionHandler(e);
+
+        continue;
+    }
+
+    Console.WriteLine("Press Enter to continue or Esc to exit\n");
+    do
+        consoleKey = Console.ReadKey(true).Key;
+    while ((consoleKey != ConsoleKey.Enter) && (consoleKey != ConsoleKey.Escape));
+    if (consoleKey == ConsoleKey.Escape)
+        Environment.Exit(0);
+}
+
+bool GetValuesFromSamplesSet(List<double> independentValues, List<double> dependentValues)
+{
+    while (true)
+    {
+        string? sampleLine = streamReader.ReadLine();
+        if (sampleLine is null)
+            return true;
+
+        string[] sampleSeparated = sampleLine.Split(',', 3);
+
+        if (sampleSeparated.Length != 2)
+        {
+            Console.WriteLine("Warning: invalid input data format");
+
+            return false;
+        }
+
+        if (!double.TryParse(sampleSeparated[0], out double independent))
+        {
+            Console.WriteLine("Warning: the independent value is not a number in the {0}th sample", independentValues.Count + 1);
+
+            return false;
+        }
+        if (!double.TryParse(sampleSeparated[1], out double dependent))
+        {
+            Console.WriteLine("Warning: the dependent value is not a number in the {0}th sample", dependentValues.Count + 1);
+
+            return false;
+        }
+        independentValues.Add(independent);
+        dependentValues.Add(dependent);
+    }
+}
+
+void ExceptionHandler(Exception e)
+{
+    Console.WriteLine("An exception has occurred:\n" + e.StackTrace);
+    Console.WriteLine($"> {e.Message}");
+    Console.WriteLine("Press Enter to continue or Esc to exit\n");
+    do
+        consoleKey = Console.ReadKey(true).Key;
+    while ((consoleKey != ConsoleKey.Enter) && (consoleKey != ConsoleKey.Escape));
+    if (consoleKey == ConsoleKey.Escape)
+        Environment.Exit(0);
+}
---
+++ Statistics/Statistics.cs
@@ -0,0 +1,54 @@
+﻿namespace Statistics
+{
+    public static class LeastSquares
+    {
+        private static (double value, int occurrences) DetectMultipleOccurrences(double[] values)
+        {
+            var set = new HashSet<double>();
+            foreach (var item in values)
+                if (!set.Add(item))
+                    return (item, values.Count(x => x == item));
+
+            return (0, 0);
+        }
+        public static (double slope, double intercept) GetSlopeAndIntercept(double[] independent, double[] dependent)
+        {
+            ArgumentNullException.ThrowIfNull(independent);
+            ArgumentNullException.ThrowIfNull(dependent);
+            if (independent.Length != dependent.Length)
+                throw new ArgumentException("Arrays of independent and dependent values must have the same length " +
+                    "corresponding to the number of samples");
+            if (independent.Length < 2)
+                throw new ArgumentException("At least 2 samples are required for further calculation");
+
+            /* Check for multiple occurrences in the input arrays */
+            double value, occurrences;
+            (value, occurrences) = DetectMultipleOccurrences(independent);
+            if (occurrences > 1)
+                throw new ArgumentException(string.Format("A number \"{0}\" appears {1} times within the independent values array", value, occurrences));
+            (value, occurrences) = DetectMultipleOccurrences(dependent);
+            if (occurrences > 1)
+                throw new ArgumentException(string.Format("A number \"{0}\" appears {1} times within the dependent values array", value, occurrences));
+
+            double slope;
+            double intercept;
+            double slopeNumerator = 0;
+            double slopeDenumerator = 0;
+            double independentAvg = independent.Average();
+            double dependentAvg = dependent.Average();
+
+            /* Calculate the slope of the regression line */
+            for (int i = 0; i < independent.Length; i++)
+            {
+                slopeNumerator += (independent[i] - independentAvg) * (dependent[i] - dependentAvg);
+                slopeDenumerator += Math.Pow(independent[i] - independentAvg, 2);
+            }
+            slope = slopeNumerator / slopeDenumerator;
+
+            /* Calculate the y-intercept */
+            intercept = dependentAvg - slope * independentAvg;
+
+            return (slope, intercept);
+        }
+    }
+}
